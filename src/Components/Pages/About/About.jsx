84970af907f291200a26f2766dfa023b8c3ad415
3b3al <owisgamal.2020@gmail.com>
2024-09-15 21:58:17 +0300

    init About us page

---
+++ src/Components/Pages/About/About.jsx
@@ -0,0 +1,15 @@
+/* eslint-disable jsx-a11y/img-redundant-alt */
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import img from './natural-selfcare-products-mockup_23-2149156147.jpg';
+import './About.css';
+
+
+const About = () => {
+  
+  
+  
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [window.location.pathname]);
+
