60fc4723cdd9fceeb14259531218f99be85ddcdc
3b3al <owisgamal.2020@gmail.com>
2024-09-22 21:40:59 +0300

    Enhance NotFound.jsx page

--- src/Components/Pages/Notfound/Notfound.jsx
+++ src/Components/Pages/Notfound/Notfound.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import img from '../../../images/404-error.jpg';
 import './Notfound.css';
 
 const Notfound = () => {
+    const navigate = useNavigate();
+
+    const goToHome = () => {
+        navigate('/');
+    };
+
     return (
-      <>
         <div className="notfound">
-          <div className="container">
-            <div className="row justify-content-center align-items-center">
-              <div className="col-lg-6">
-                <h1 className="mb-5">SOrry... This Page Not Found</h1>
-              </div>
-              <div className="col-lg-6">
-                <img
-                  src={img}
-                  alt=""
-                  className='img-fluid'
-                />
-              </div>
+            <div className="container text-center">
+                <div className="row justify-content-center align-items-center">
+                    <div className="col-lg-6 text-lg-end">
+                        <h1 className="mb-4">Oops! Page Not Found</h1>
+                        <p className="mb-5">Sorry, the page you're looking for doesn't exist.</p>
+                        <button className="btn btn-primary" onClick={goToHome}>
+                            Go Back Home
+                        </button>
+                    </div>
+                    <div className="col-lg-6 text-lg-start">
+                        <img src={img} alt="404 Error" className="img-fluid notfound-img" />
+                    </div>
+                </div>
             </div>
-          </div>
         </div>
-      </>
     );
-}
-
-
+};
 
 export default Notfound;
